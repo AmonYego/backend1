d8994a51cbb01dc7e886d022c3e098b737eac874
Amon <amonspiffy@gmail.com>
2025-10-26 17:12:01 +0300

    b

--- services/ai_engine.py
+++ services/ai_engine.py
@@ -6,7 +6,7 @@ from ..services.prompts import (
 )
 import google.generativeai as genai
 from fastapi import UploadFile
-from ..services.file_reader import extract_text
+from services.file_reader import extract_text
 
 genai.configure(api_key="AIzaSyARKbi8gr-3sLsw5KOEsZMUsudHA53sxBA")
 model = genai.GenerativeModel("gemini-2.5-flash")
